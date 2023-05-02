141c2da3de0f4c3653fbe15eaeae94ed2e1236a7
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-02 20:02:38 -0300

    system design file

---
+++ Touchdown/Utils/SystemDesign.swift
@@ -0,0 +1,14 @@
+//
+//  SystemDesign.swift
+//  Touchdown
+//
+//  Created by Giovanna Moeller on 02/05/23.
+//
+
+import SwiftUI
+
+struct Colors {
+  static let colorBackground = Color("ColorBackground")
+  static let colorGray = Color(UIColor.systemGray4)
+}
+
