0dff0c102d7fd11f2042fcf25d8fa5d187216c71
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-02 18:42:52 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Giovanna Moeller on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
