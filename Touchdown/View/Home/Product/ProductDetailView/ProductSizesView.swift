7ab90ccee7684da345f4aee32d5125ef2fa86974
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-03 01:08:32 -0300

    creating product rating view

---
+++ Touchdown/View/Home/Product/ProductDetailView/ProductSizesView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductSizesView.swift
+//  Touchdown
+//
+//  Created by Giovanna Moeller on 03/05/23.
+//
+
+import SwiftUI
+
+struct ProductSizesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductSizesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductSizesView()
+    }
+}
