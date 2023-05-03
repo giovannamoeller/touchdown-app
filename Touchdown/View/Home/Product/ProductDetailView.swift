0f7bf50d2959f203a6f383db47967b64593f02af
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-03 00:41:02 -0300

    developing product detail view - inserting title, price, image and description

--- Touchdown/View/Home/Product/ProductDetailView.swift
+++ Touchdown/View/Home/Product/ProductDetailView.swift
@@ -12,11 +12,54 @@ struct ProductDetailView: View {
   // MARK: - PROPERTIES
   
   let product: Product
+  var backgroundColor: Color {
+    return Color(red: product.color[0], green: product.color[1], blue: product.color[2])
+  }
   
   // MARK: - BODY
   
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ZStack {
+      backgroundColor.ignoresSafeArea()
+      
+      VStack(alignment: .leading) {
+        Text("Protective Gear")
+          .foregroundColor(.white)
+        
+        Text(product.name)
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .foregroundColor(.white)
+        
+        Text("Price")
+          .foregroundColor(.black)
+        
+        Text("$\(product.price)")
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .foregroundColor(.black)
+        
+        ZStack(alignment: .topTrailing) {
+          VStack {
+            Spacer()
+            
+            Text(product.description)
+            
+            Spacer()
+          } //: VSTACK
+          .padding()
+          .background(Color.white)
+          .cornerRadius(32)
+          .ignoresSafeArea()
+          
+          Image(product.image)
+            .resizable()
+            .scaledToFit()
+            .frame(maxWidth: 186)
+            .offset(x: -16, y: -102)
+        } //: ZSTACK
+      } //: VSTACK
+    } //: ZSTACK
   }
 }
 
