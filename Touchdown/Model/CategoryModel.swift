dee9af15dc8a05de846bcd03b2d3c79b9989a026
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-02 22:43:38 -0300

    category model

---
+++ Touchdown/Model/CategoryModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryModel.swift
+//  Touchdown
+//
+//  Created by Giovanna Moeller on 02/05/23.
+//
+
+import Foundation
+
+struct Category: Identifiable, Codable {
+  let id: Int
+  let name: String
+  let image: String
+}
