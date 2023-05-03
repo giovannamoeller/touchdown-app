163b7628dc4812f591fe91596d3c775d5ecbbb24
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-02 22:45:48 -0300

    implementing layout structure in system design

--- Touchdown/Utils/SystemDesign.swift
+++ Touchdown/Utils/SystemDesign.swift
@@ -12,3 +12,10 @@ struct Colors {
   static let colorGray = Color(UIColor.systemGray4)
 }
 
+struct Layout {
+  static let columnSpacing: CGFloat = 10
+  static let rowSpacing: CGFloat = 10
+  static var gridLayout: [GridItem] {
+    return Array(repeating: GridItem(.flexible(), spacing: rowSpacing), count: 2)
+  }
+}
