19b23539efe3e6f3cac63c5ffe76f58af3ff0b58
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-02 22:52:43 -0300

    category item view

---
+++ Touchdown/View/Home/CategoryItemView.swift
@@ -0,0 +1,46 @@
+//
+//  CategoryItemView.swift
+//  Touchdown
+//
+//  Created by Giovanna Moeller on 02/05/23.
+//
+
+import SwiftUI
+
+struct CategoryItemView: View {
+  
+  // MARK: - PROPERTIES
+  
+  let category: Category
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    Button {
+      //
+    } label: {
+      HStack(alignment: .center, spacing: 6) {
+        Image(category.image)
+          .resizable()
+          .scaledToFit()
+          .frame(width: 32, height: 32)
+        
+        Text(category.name.uppercased())
+          .foregroundColor(.gray)
+      } //: HSTACK
+      .padding()
+      .background(Color.white.cornerRadius(16))
+      .background(
+        RoundedRectangle(cornerRadius: 16).stroke(.gray, style: .init(lineWidth: 1.5))
+      )
+      
+    } //: BUTTON
+  }
+}
+
+struct CategoryItemView_Previews: PreviewProvider {
+  static let categories: [Category] = DataManager.decode("category")
+  static var previews: some View {
+    CategoryItemView(category: categories[0])
+  }
+}
