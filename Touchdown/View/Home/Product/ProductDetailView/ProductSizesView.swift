fc8853aa09e41dc5827e73d749e265e3381bb8e6
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-03 01:13:14 -0300

    creating ProductSizesView

--- Touchdown/View/Home/Product/ProductDetailView/ProductSizesView.swift
+++ Touchdown/View/Home/Product/ProductDetailView/ProductSizesView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ProductSizesView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  
+  // MARK: - PROPERTIES
+  
+  let sizes: [String] = ["XS", "S", "M", "L", "XL"]
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    VStack(alignment: .trailing, spacing: 8) {
+      Text("Sizes")
+        .bold()
+      
+      HStack(spacing: 8) {
+        ForEach(sizes, id: \.self) { size in
+          Text(size)
+            .font(.title3)
+            .bold()
+            .frame(width: 42, height: 42)
+            .background(
+              RoundedRectangle(cornerRadius: 8).stroke(Colors.colorGray, lineWidth: 1.5)
+            )
+        }
+      }
+    } //: VSTACK
+    .foregroundColor(Colors.colorGray)
+  }
 }
 
 struct ProductSizesView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProductSizesView()
-    }
+  static var previews: some View {
+    ProductSizesView()
+  }
 }
